aea430a8d350c704254cc2efd8dac73e3553a75a
G1anpierre <fgianpierre@gmail.com>
2023-10-08 17:44:16 +0200

    small modification to error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -16,7 +16,7 @@ export default function Error({
 
   return (
     <div className="relative h-screen flex justify-center items-center">
-      <div>
+      <div className="flex flex-col justify-center">
         <h2>Something went wrong!</h2>
         <button
           className="bg-indigo-500 rounded-md px-3.5 py-2.5 text-sm text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
