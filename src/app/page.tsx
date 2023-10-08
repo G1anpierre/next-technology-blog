b24918bc61b5e9fc28e084d5c409f997d931dbed
G1anpierre <fgianpierre@gmail.com>
2023-10-08 16:52:05 +0200

    delete unnesesary query in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,8 @@
 import {FeaturePosts} from '@/components/FeaturePosts'
 import {Hero} from '@/components/Hero'
 import {RecentPosts} from '@/components/RecentPosts'
-import {getClient} from '@/lib/client'
-import {gql} from '@apollo/client'
-
-const query = gql`
-  query {
-    posts {
-      data {
-        attributes {
-          title
-          author
-          body
-          author
-          slug
-          categories {
-            data {
-              attributes {
-                name
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
 
 export default async function Home() {
-  const client = getClient()
-  const {data} = await client.query({
-    query,
-    context: {
-      fetchOptions: {
-        next: {revalidate: 5},
-      },
-    },
-  })
-
   return (
     <main>
       <Hero />
