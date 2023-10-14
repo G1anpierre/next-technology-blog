919ced77004a04af66e56c8381c565944e189868
G1anpierre <fgianpierre@gmail.com>
2023-10-14 15:12:11 +0200

    disable button while processing form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -200,7 +200,8 @@ export const ContactForm = () => {
       <div className="mt-10 flex justify-end border-t border-gray-900/10 pt-8">
         <button
           type="submit"
-          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-75"
+          disabled={formik.isSubmitting || !formik.isValid}
         >
           Send message
         </button>
