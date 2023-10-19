b964d37fb266c4d4f3f2198408602d2e232f21dd
G1anpierre <fgianpierre@gmail.com>
2023-10-19 09:58:24 +0200

    use component Image in contact

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,5 +1,6 @@
 import {ContactForm} from '@/components/ContactForm'
 import React from 'react'
+import Image from 'next/image'
 
 export const metadata = {
   title: 'Contact',
@@ -10,10 +11,12 @@ export default async function Contact() {
   return (
     <div className="relative bg-white">
       <div className="lg:absolute lg:inset-0 lg:left-1/2">
-        <img
+        <Image
           className="h-64 w-full bg-gray-50 object-cover sm:h-80 lg:absolute lg:h-full"
           src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=2560&h=3413&&q=80"
-          alt=""
+          alt="people working on laptops"
+          width={1920}
+          height={2560}
         />
       </div>
       <div className="pb-24 pt-16 sm:pb-32 sm:pt-24 lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:pt-32">
